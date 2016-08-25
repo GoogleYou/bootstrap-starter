70def9d591bd66cd36f66c535a1eeea5eca6ac04
Andre <a.schledermann@gmx.de>
2016-08-25 14:58:43 +0200

    AS

--- app/js/main.js
+++ app/js/main.js
@@ -4,3 +4,15 @@ DB.connect(app);
 DB.ready(function() {
     $('#hello').html('Connected to Baqend app <strong>' + app + "</strong>");
 });
+
+DB.ready(function () {
+    $('#123').click(function () {
+        $("#234").hide(5000);
+    })
+});
+
+DB.ready(function () {
+    $('#234').click(function () {
+        $("#123").hide(5000);
+    })
+});
