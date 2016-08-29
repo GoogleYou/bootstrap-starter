65db234351f28afe915089881d1f79df25ac9ffe
Frank <frank.nehler@gmx.de>
2016-08-29 17:45:35 +0200

    Seter für Competition, Design und Category

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,37 @@
 var app = 'hallo';
-DB.connect(app);
+DB.connect(app).then()
+{
+    function setCategory(name, limit, id) {
+        var category = new DB.Category;
+        category.name = name;
+        category.voteLimitPerPerson = limit;
+        category.idCategory = id;
+        category.save();
+    }
 
-DB.ready(function() {
-    $('#hello').html('Connected to Baqend app <strong>' + app + "</strong>");
-});
+    function setCompetition(id, season, theme, ideaFrom, category, time) {
+        var competition = new DB.Competition;
+        competition.idCompetition = id;
+        competition.season = season;
+        competition.theme = theme;
+        competition.ideaFrom = ideaFrom;
+        competition.category = category;
+        competition.time = time;
+        competition.save();
+    }
 
-DB.ready(function () {
-    $('#123').click(function () {
-        $("#234").hide(5000);
-    })
-});
-
-DB.ready(function () {
-    $('#234').click(function () {
-        $("#123").hide(5000);
-        $("#345").hide(5000);
-    })
-});
-
-
-/// hiiii
+    function setDesign(id, designer, color, galery, material, information, voteCounter, competitionId, category, comment) {
+        var design = new DB.Design;
+        design.idDesign = id;
+        design.designer = designer;
+        design.color = color;
+        design.galery = galery;
+        design.material = material;
+        design.information = information;
+        design.voteCounter = voteCounter;
+        design.competitionId = competitionId;
+        design.category = category;
+        design.comment = comment;
+        design.save();
+    }
+};
