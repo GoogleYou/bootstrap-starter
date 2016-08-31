f25216ad7dc071947f5a5c5bba4f1ce50475b9ea
Frank <frank.nehler@gmx.de>
2016-08-31 15:59:00 +0200

    FiFaFeli

--- app/js/main.js
+++ app/js/main.js
@@ -23,7 +23,7 @@ function setCompetition(id, season, theme, ideaFrom, category, time) {
     competition.save();
 }
 
-function setDesign(id, designer, color, galery, material, information, voteCounter, competitionId, category, comment) {
+function setDesign(id, designer, color, galery, material, information, voteCounter, competitionId, category, comment){
     var design = new DB.Design;
     design.idDesign = id;
     design.designer = designer;
