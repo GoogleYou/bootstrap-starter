a53626e664871efd132b9b70fd116b15cee5e80d
felicitascastrian <feli-castrian@msn.om>
2016-09-06 13:34:39 +0200

    bu

--- app/js/votingPageService.js
+++ app/js/votingPageService.js
@@ -25,10 +25,11 @@ function findDesigns(cat) {
 $("#Shirtsbtn").click(function () {
     DB.Design.find().matches('category',/^Shirts/ ).resultList(function (result) {
         {
-            result.forEach(function (inf) {
-                var bildUrl = inf.gallery[0];
-                $('#testest')
-                    .replaceWith("<div class='col-xs-4 col-sm-4 col-md-3 col-lg-3><div class='img-thumbnail img-responsive'><a>" +
+            $('#voting-gallery-container').empty();
+            result.forEach(function (designs) {
+                var bildUrl = designs.gallery[0];
+                $('#voting-gallery-container')
+                    .append("<div class='col-xs-4 col-sm-4 col-md-3 col-lg-3><div class='img-thumbnail img-responsive'><a>" +
                             "<img class='imgScaling' src='" + bildUrl +
                             "'></a> " +
                             "<div class='desc'><button type='button' class='btnvote' aria-label='Left Align' id='Votebtn'>" +
@@ -47,10 +48,11 @@ $("#Shirtsbtn").click(function () {
 $("#Pulloverbtn").click(function () {
     DB.Design.find().matches('category',/^Pullover/ ).resultList(function (result) {
         {
-            result.forEach(function (inf) {
-                var bildUrl = inf.gallery[0];
-                $('#testest')
-                    .replaceWith("<div class='col-xs-4 col-sm-4 col-md-3 col-lg-3><div class='img-thumbnail img-responsive'><a>" +
+            $('#voting-gallery-container').empty();
+            result.forEach(function (designs) {
+                var bildUrl = designs.gallery[0];
+                $('#voting-gallery-container')
+                    .append("<div class='col-xs-4 col-sm-4 col-md-3 col-lg-3><div class='img-thumbnail img-responsive'><a>" +
                             "<img class='imgScaling' src='" + bildUrl +
                             "'></a> " +
                             "<div class='desc'><button type='button' class='btnvote' aria-label='Left Align' id='Votebtn'>" +
@@ -68,10 +70,11 @@ $("#Pulloverbtn").click(function () {
 $("#Jacketsbtn").click(function () {
     DB.Design.find().matches('category',/^Jackets/ ).resultList(function (result) {
         {
-            result.forEach(function (inf) {
-                var bildUrl = inf.gallery[0];
-                $('#testest')
-                    .replaceWith("<div class='col-xs-4 col-sm-4 col-md-3 col-lg-3><div class='img-thumbnail img-responsive'><a>" +
+            $('#voting-gallery-container').empty();
+            result.forEach(function (designs) {
+                var bildUrl = designs.gallery[0];
+                $('#voting-gallery-container')
+                    .append("<div class='col-xs-4 col-sm-4 col-md-3 col-lg-3><div class='img-thumbnail img-responsive'><a>" +
                             "<img class='imgScaling' src='" + bildUrl +
                             "'></a> " +
                             "<div class='desc'><button type='button' class='btnvote' aria-label='Left Align' id='Votebtn'>" +
@@ -89,10 +92,11 @@ $("#Jacketsbtn").click(function () {
 $("#Specialsbtn").click(function () {
     DB.Design.find().matches('category',/^Specials/ ).resultList(function (result) {
         {
-            result.forEach(function (inf) {
-                var bildUrl = inf.gallery[0];
-                $('#testest')
-                    .replaceWith("<div class='col-xs-4 col-sm-4 col-md-3 col-lg-3><div class='img-thumbnail img-responsive'><a>" +
+            $('#voting-gallery-container').empty();
+            result.forEach(function (designs) {
+                var bildUrl = designs.gallery[0];
+                $('#voting-gallery-container')
+                    .append("<div class='col-xs-4 col-sm-4 col-md-3 col-lg-3><div class='img-thumbnail img-responsive'><a>" +
                             "<img class='imgScaling' src='" + bildUrl +
                             "'></a> " +
                             "<div class='desc'><button type='button' class='btnvote' aria-label='Left Align' id='Votebtn'>" +
@@ -128,23 +132,6 @@ function displayDesigns() {
                     "<span class='glyphicon glyphicon-zoom-in'></span> Zoom in" +
                     "</a></button></div></div>")
         ;
-
-        /*  <div class="col-xs-6 col-sm-3">
-         <div class="img-thumbnail img-responsive">
-         <a href="https://hallo-bq.global.ssl.fastly.net/v1/file/www/img/Tshirt2.png?BCB" data-lightbox="TestBild"><img src="https://hallo-bq.global.ssl.fastly.net/v1/file/www/img/Tshirt2.png?BCB" width="300"
-         height="200"></a>
-         <div class="desc">
-         <button type="button" class="btnvote" aria-label="Left Align" id="Votebtn">
-         <span class="glyphicon glyphicon-heart"></span> Vote
-         </button>
-         <button type="button" class="btnzoom" aria-label="Right Align">
-         <a href="https://hallo-bq.global.ssl.fastly.net/v1/file/www/img/Tshirt2.png?BCB">
-         <span class="glyphicon glyphicon-zoom-in"></span> Zoom in
-         </a>
-         </button>
-         </div>
-         </div>
-         </div>   */
     }
 }
 
@@ -152,9 +139,10 @@ function displayDesigns() {
 DB.ready(function () {
     DB.Design.find().resultList(function (result) {
         {
-            result.forEach(function (inf) {
-                var bildUrl = inf.gallery[0];
-                $('#testest')
+            $('#voting-gallery-container').empty();
+            result.forEach(function (designs) {
+                var bildUrl = designs.gallery[0];
+                $('#voting-gallery-container')
                     .append("<div class='col-xs-4 col-sm-4 col-md-3 col-lg-3><div class='img-thumbnail img-responsive'><a>" +
                             "<img class='imgScaling' src='" + bildUrl +
                             "'></a> " +
