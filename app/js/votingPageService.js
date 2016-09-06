65dd15820df90148bb4dbb0349590df28595dfe2
Frank <frank.nehler@gmx.de>
2016-09-06 13:24:09 +0200

    FiFaFeli

--- app/js/votingPageService.js
+++ app/js/votingPageService.js
@@ -23,14 +23,13 @@ function findDesigns(cat) {
 }
 
 $("#Shirtsbtn").click(function () {
-    elements = [];
-    findDesigns("Shirts");
     DB.Design.find().matches('category',/^Shirts/ ).resultList(function (result) {
         {
-            result.forEach(function (inf) {
-                var bildUrl = inf.gallery[0];
-                $('#testest')
-                    .replaceWith("<dic class='col-xs-4 col-sm-4 col-md-3 col-lg-3><div class='img-thumbnail img-responsive'><a>" +
+            $('#voting-gallery-container').empty();
+            result.forEach(function (designs) {
+                var bildUrl = designs.gallery[0];
+                $('#voting-gallery-container')
+                    .append("<dic class='col-xs-4 col-sm-4 col-md-3 col-lg-3><div class='img-thumbnail img-responsive'><a>" +
                             "<img class='imgScaling' src='" + bildUrl +
                             "'></a> " +
                             "<div class='desc'><button type='button' class='btnvote' aria-label='Left Align' id='Votebtn'>" +
@@ -47,69 +46,67 @@ $("#Shirtsbtn").click(function () {
 
 
 $("#Pulloverbtn").click(function () {
-    elements = [];
-    findDesigns("Pullover");
-    DB.Design.find().matches('category',/^Pullover/ ).resultList(function (result) {
+    DB.Design.find().matches('category',/^Pullover/ ).resultList(function (result)  {
         {
-            result.forEach(function (inf) {
-                var bildUrl = inf.gallery[0];
-                $('#testest')
-                    .replaceWith("<dic class='col-xs-4 col-sm-4 col-md-3 col-lg-3><div class='img-thumbnail img-responsive'><a>" +
-                            "<img class='imgScaling' src='" + bildUrl +
-                            "'></a> " +
-                            "<div class='desc'><button type='button' class='btnvote' aria-label='Left Align' id='Votebtn'>" +
-                            "<span class='glyphicon glyphicon-heart'></span> Vote " +
-                            "</button>" +
-                            "<button type='button' class='btnzoom' aria-lable='Right Align'>" +
-                            "<a href='" + bildUrl + "'data-lightbox='TestBild'>" +
-                            "<span class='glyphicon glyphicon-zoom-in'></span> Zoom in" +
-                            "</a></button></div></div>")
+            $('#voting-gallery-container').empty();
+            result.forEach(function (designs) {
+                var bildUrl = designs.gallery[0];
+                $('#voting-gallery-container')
+                    .append("<dic class='col-xs-4 col-sm-4 col-md-3 col-lg-3><div class='img-thumbnail img-responsive'><a>" +
+                        "<img class='imgScaling' src='" + bildUrl +
+                        "'></a> " +
+                        "<div class='desc'><button type='button' class='btnvote' aria-label='Left Align' id='Votebtn'>" +
+                        "<span class='glyphicon glyphicon-heart'></span> Vote " +
+                        "</button>" +
+                        "<button type='button' class='btnzoom' aria-lable='Right Align'>" +
+                        "<a href='" + bildUrl + "'data-lightbox='TestBild'>" +
+                        "<span class='glyphicon glyphicon-zoom-in'></span> Zoom in" +
+                        "</a></button></div></div>")
             })
         }
     })
 });
 
+
 $("#Jacketsbtn").click(function () {
-    elements = [];
-    findDesigns("Jackets");
-    DB.Design.find().matches('category',/^Jackets/ ).resultList(function (result) {
-        {
-            result.forEach(function (inf) {
-                var bildUrl = inf.gallery[0];
-                $('#testest')
-                    .replaceWith("<dic class='col-xs-4 col-sm-4 col-md-3 col-lg-3><div class='img-thumbnail img-responsive'><a>" +
-                            "<img class='imgScaling' src='" + bildUrl +
-                            "'></a> " +
-                            "<div class='desc'><button type='button' class='btnvote' aria-label='Left Align' id='Votebtn'>" +
-                            "<span class='glyphicon glyphicon-heart'></span> Vote " +
-                            "</button>" +
-                            "<button type='button' class='btnzoom' aria-lable='Right Align'>" +
-                            "<a href='" + bildUrl + "'data-lightbox='TestBild'>" +
-                            "<span class='glyphicon glyphicon-zoom-in'></span> Zoom in" +
-                            "</a></button></div></div>")
-            })
-        }
-    })
+   DB.Design.find().matches('category',/^Jackets/ ).resultList(function (result)   {
+       {
+           $('#voting-gallery-container').empty();
+           result.forEach(function (designs) {
+               var bildUrl = designs.gallery[0];
+               $('#voting-gallery-container')
+                   .append("<dic class='col-xs-4 col-sm-4 col-md-3 col-lg-3><div class='img-thumbnail img-responsive'><a>" +
+                       "<img class='imgScaling' src='" + bildUrl +
+                       "'></a> " +
+                       "<div class='desc'><button type='button' class='btnvote' aria-label='Left Align' id='Votebtn'>" +
+                       "<span class='glyphicon glyphicon-heart'></span> Vote " +
+                       "</button>" +
+                       "<button type='button' class='btnzoom' aria-lable='Right Align'>" +
+                       "<a href='" + bildUrl + "'data-lightbox='TestBild'>" +
+                       "<span class='glyphicon glyphicon-zoom-in'></span> Zoom in" +
+                       "</a></button></div></div>")
+           })
+       }
+   })
 });
 
 $("#Specialsbtn").click(function () {
-    elements = [];
-    findDesigns("Specials");
-    DB.Design.find().matches('category',/^Specials/ ).resultList(function (result) {
+    DB.Design.find().matches('category',/^Specials/ ).resultList(function (result)   {
         {
-            result.forEach(function (inf) {
-                var bildUrl = inf.gallery[0];
-                $('#testest')
-                    .replaceWith("<dic class='col-xs-4 col-sm-4 col-md-3 col-lg-3><div class='img-thumbnail img-responsive'><a>" +
-                            "<img class='imgScaling' src='" + bildUrl +
-                            "'></a> " +
-                            "<div class='desc'><button type='button' class='btnvote' aria-label='Left Align' id='Votebtn'>" +
-                            "<span class='glyphicon glyphicon-heart'></span> Vote " +
-                            "</button>" +
-                            "<button type='button' class='btnzoom' aria-lable='Right Align'>" +
-                            "<a href='" + bildUrl + "'data-lightbox='TestBild'>" +
-                            "<span class='glyphicon glyphicon-zoom-in'></span> Zoom in" +
-                            "</a></button></div></div>")
+            $('#voting-gallery-container').empty();
+            result.forEach(function (designs) {
+                var bildUrl = designs.gallery[0];
+                $('#voting-gallery-container')
+                    .append("<dic class='col-xs-4 col-sm-4 col-md-3 col-lg-3><div class='img-thumbnail img-responsive'><a>" +
+                        "<img class='imgScaling' src='" + bildUrl +
+                        "'></a> " +
+                        "<div class='desc'><button type='button' class='btnvote' aria-label='Left Align' id='Votebtn'>" +
+                        "<span class='glyphicon glyphicon-heart'></span> Vote " +
+                        "</button>" +
+                        "<button type='button' class='btnzoom' aria-lable='Right Align'>" +
+                        "<a href='" + bildUrl + "'data-lightbox='TestBild'>" +
+                        "<span class='glyphicon glyphicon-zoom-in'></span> Zoom in" +
+                        "</a></button></div></div>")
             })
         }
     })
@@ -121,43 +118,9 @@ $("#Votebtn").click(function () {
 
 $("#1234").text(localStorage.getItem("competitionName"));
 
-function displayDesigns() {
-    for (i = 0; i < elements.length; i++)
-    {
-        var url = elements[i].url;
-        $("#galleryContainer")
-            .append("<dic class='col-xs-6 col-sm-3'><div class='img-thumbnail img-responsive'><a><img src='" + bildUrl +
-                    "' width='300' height='200'></a> " +
-                    "<div class='desc'><button type='button' class='btnvote' aria-label='Left Align' id='Votebtn'>" +
-                    "<span class='glyphicon glyphicon-heart'></span> Vote " +
-                    "</button>" +
-                    "<button type='button' class='btnzoom' aria-lable='Right Align'>" +
-                    "<a href='" + url + "'data-lightbox='TestBild'>" +
-                    "<span class='glyphicon glyphicon-zoom-in'></span> Zoom in" +
-                    "</a></button></div></div>")
-        ;
-
-        /*  <div class="col-xs-6 col-sm-3">
-         <div class="img-thumbnail img-responsive">
-         <a href="https://hallo-bq.global.ssl.fastly.net/v1/file/www/img/Tshirt2.png?BCB" data-lightbox="TestBild"><img src="https://hallo-bq.global.ssl.fastly.net/v1/file/www/img/Tshirt2.png?BCB" width="300"
-         height="200"></a>
-         <div class="desc">
-         <button type="button" class="btnvote" aria-label="Left Align" id="Votebtn">
-         <span class="glyphicon glyphicon-heart"></span> Vote
-         </button>
-         <button type="button" class="btnzoom" aria-label="Right Align">
-         <a href="https://hallo-bq.global.ssl.fastly.net/v1/file/www/img/Tshirt2.png?BCB">
-         <span class="glyphicon glyphicon-zoom-in"></span> Zoom in
-         </a>
-         </button>
-         </div>
-         </div>
-         </div>   */
-    }
-}
 
 
-DB.ready(function () {
+/*DB.ready(function () {
     DB.Design.find().resultList(function (result) {
         {
             result.forEach(function (inf) {
@@ -176,4 +139,18 @@ DB.ready(function () {
             })
         }
     })
-});
+}); */
+function showDesigns() {
+    $('#voting-gallery-container')
+        .append("<dic class='col-xs-4 col-sm-4 col-md-3 col-lg-3><div class='img-thumbnail img-responsive'><a>" +
+            "<img class='imgScaling' src='" + bildUrl +
+            "'></a> " +
+            "<div class='desc'><button type='button' class='btnvote' aria-label='Left Align' id='Votebtn'>" +
+            "<span class='glyphicon glyphicon-heart'></span> Vote " +
+            "</button>" +
+            "<button type='button' class='btnzoom' aria-lable='Right Align'>" +
+            "<a href='" + bildUrl + "'data-lightbox='TestBild'>" +
+            "<span class='glyphicon glyphicon-zoom-in'></span> Zoom in" +
+            "</a></button></div></div>")
+}
+
