18ce008d3ecc120686f304561cd22189840dfe46
Frank <frank.nehler@gmx.de>
2016-09-06 13:49:28 +0200

    FiFaFeli

--- app/js/votingPageService.js
+++ app/js/votingPageService.js
@@ -4,80 +4,13 @@
 
 
 $("#Shirtsbtn").click(function () {
-    DB.Design.find().matches('category',/^Shirts/ ).resultList(function (result) {
+    DB.Design.find().matches('category', /^Shirts/).resultList(function (result) {
         {
             $('#voting-gallery-container').empty();
             result.forEach(function (designs) {
                 var bildUrl = designs.gallery[0];
                 $('#voting-gallery-container')
                     .append("<div class='col-xs-4 col-sm-4 col-md-3 col-lg-3'><div class='img-thumbnail img-responsive'><a>" +
-                            "<img class='imgScaling' src='" + bildUrl +
-                            "'></a> " +
-                            "<div class='desc'><button type='button' class='btnvote' aria-label='Left Align' id='Votebtn'>" +
-                            "<span class='glyphicon glyphicon-heart'></span> Vote " +
-                            "</button>" +
-                            "<button type='button' class='btnzoom' aria-lable='Right Align'>" +
-                            "<a href='" + bildUrl + "'data-lightbox='TestBild'>" +
-                            "<span class='glyphicon glyphicon-zoom-in'></span> Zoom in" +
-                            "</a></button></div></div>")
-            })
-        }
-    })
-});
-
-
-$("#Pulloverbtn").click(function () {
-    DB.Design.find().matches('category',/^Pullover/ ).resultList(function (result) {
-        {
-            $('#voting-gallery-container').empty();
-            result.forEach(function (designs) {
-                var bildUrl = designs.gallery[0];
-                $('#voting-gallery-container')
-                    .append("<div class='col-xs-4 col-sm-4 col-md-3 col-lg-3'><div class='img-thumbnail img-responsive'><a>" +
-                            "<img class='imgScaling' src='" + bildUrl +
-                            "'></a> " +
-                            "<div class='desc'><button type='button' class='btnvote' aria-label='Left Align' id='Votebtn'>" +
-                            "<span class='glyphicon glyphicon-heart'></span> Vote " +
-                            "</button>" +
-                            "<button type='button' class='btnzoom' aria-lable='Right Align'>" +
-                            "<a href='" + bildUrl + "'data-lightbox='TestBild'>" +
-                            "<span class='glyphicon glyphicon-zoom-in'></span> Zoom in" +
-                            "</a></button></div></div>")
-            })
-        }
-    })
-});
-
-$("#Jacketsbtn").click(function () {
-    DB.Design.find().matches('category',/^Jackets/ ).resultList(function (result) {
-        {
-            $('#voting-gallery-container').empty();
-            result.forEach(function (designs) {
-                var bildUrl = designs.gallery[0];
-                $('#voting-gallery-container')
-                    .append("<div class='col-xs-4 col-sm-4 col-md-3 col-lg-3'><div class='img-thumbnail img-responsive'><a>" +
-                            "<img class='imgScaling' src='" + bildUrl +
-                            "'></a> " +
-                            "<div class='desc'><button type='button' class='btnvote' aria-label='Left Align' id='Votebtn'>" +
-                            "<span class='glyphicon glyphicon-heart'></span> Vote " +
-                            "</button>" +
-                            "<button type='button' class='btnzoom' aria-lable='Right Align'>" +
-                            "<a href='" + bildUrl + "'data-lightbox='TestBild'>" +
-                            "<span class='glyphicon glyphicon-zoom-in'></span> Zoom in" +
-                            "</a></button></div></div>")
-            })
-        }
-    })
-});
-
-$("#Specialsbtn").click(function () {
-    DB.Design.find().matches('category',/^Specials/ ).resultList(function (result){
-        {
-        $('#voting-gallery-container').empty();
-        result.forEach(function (designs) {
-            var bildUrl = designs.gallery[0];
-            $('#voting-gallery-container')
-                .append("<div class='col-xs-4 col-sm-4 col-md-3 col-lg-3'><div class='img-thumbnail img-responsive'><a>" +
                         "<img class='imgScaling' src='" + bildUrl +
                         "'></a> " +
                         "<div class='desc'><button type='button' class='btnvote' aria-label='Left Align' id='Votebtn'>" +
@@ -87,25 +20,90 @@ $("#Specialsbtn").click(function () {
                         "<a href='" + bildUrl + "'data-lightbox='TestBild'>" +
                         "<span class='glyphicon glyphicon-zoom-in'></span> Zoom in" +
                         "</a></button></div></div>")
-        })
-    }
-
-})
+            })
+        }
+    })
 });
 
 
+$("#Pulloverbtn").click(function () {
+    DB.Design.find().matches('category', /^Pullover/).resultList(function (result) {
+        {
+            $('#voting-gallery-container').empty();
+            result.forEach(function (designs) {
+                var bildUrl = designs.gallery[0];
+                $('#voting-gallery-container')
+                    .append("<div class='col-xs-4 col-sm-4 col-md-3 col-lg-3'><div class='img-thumbnail img-responsive'><a>" +
+                        "<img class='imgScaling' src='" + bildUrl +
+                        "'></a> " +
+                        "<div class='desc'><button type='button' class='btnvote' aria-label='Left Align' id='Votebtn'>" +
+                        "<span class='glyphicon glyphicon-heart'></span> Vote " +
+                        "</button>" +
+                        "<button type='button' class='btnzoom' aria-lable='Right Align'>" +
+                        "<a href='" + bildUrl + "'data-lightbox='TestBild'>" +
+                        "<span class='glyphicon glyphicon-zoom-in'></span> Zoom in" +
+                        "</a></button></div></div>")
+            })
+        }
+    })
+});
+
+$("#Jacketsbtn").click(function () {
+    DB.Design.find().matches('category', /^Jackets/).resultList(function (result) {
+        {
+            $('#voting-gallery-container').empty();
+            result.forEach(function (designs) {
+                var bildUrl = designs.gallery[0];
+                $('#voting-gallery-container')
+                    .append("<div class='col-xs-4 col-sm-4 col-md-3 col-lg-3'><div class='img-thumbnail img-responsive'><a>" +
+                        "<img class='imgScaling' src='" + bildUrl +
+                        "'></a> " +
+                        "<div class='desc'><button type='button' class='btnvote' aria-label='Left Align' id='Votebtn'>" +
+                        "<span class='glyphicon glyphicon-heart'></span> Vote " +
+                        "</button>" +
+                        "<button type='button' class='btnzoom' aria-lable='Right Align'>" +
+                        "<a href='" + bildUrl + "'data-lightbox='TestBild'>" +
+                        "<span class='glyphicon glyphicon-zoom-in'></span> Zoom in" +
+                        "</a></button></div></div>")
+            })
+        }
+    })
+});
+
+$("#Specialsbtn").click(function () {
+    DB.Design.find().matches('category', /^Specials/).resultList(function (result) {
+        {
+            $('#voting-gallery-container').empty();
+            result.forEach(function (designs) {
+                var bildUrl = designs.gallery[0];
+                $('#voting-gallery-container')
+                    .append("<div class='col-xs-4 col-sm-4 col-md-3 col-lg-3'><div class='img-thumbnail img-responsive'><a>" +
+                        "<img class='imgScaling' src='" + bildUrl +
+                        "'></a> " +
+                        "<div class='desc'><button type='button' class='btnvote' aria-label='Left Align' id='Votebtn'>" +
+                        "<span class='glyphicon glyphicon-heart'></span> Vote " +
+                        "</button>" +
+                        "<button type='button' class='btnzoom' aria-lable='Right Align'>" +
+                        "<a href='" + bildUrl + "'data-lightbox='TestBild'>" +
+                        "<span class='glyphicon glyphicon-zoom-in'></span> Zoom in" +
+                        "</a></button></div></div>")
+            })
+        }
+
+    })
+});
+
 
 $("#Votebtn").click(function () {
-                    }
+    }
 );
 
 $("#1234").text(localStorage.getItem("competitionName"));
 
 
-
 DB.ready(function () {
     DB.Design.find().resultList(function (result) {
-        DB.Design.find().matches('category',/^Shirts/ ).resultList(function (result) {
+        DB.Design.find().matches('category', /^Shirts/).resultList(function (result) {
             {
                 $('#voting-gallery-container').empty();
                 result.forEach(function (designs) {
@@ -121,10 +119,10 @@ DB.ready(function () {
                             "<a href='" + bildUrl + "'data-lightbox='TestBild'>" +
                             "<span class='glyphicon glyphicon-zoom-in'></span> Zoom in" +
                             "</a></button></div></div>")
-            })
-        }
-    })
-});
+                })
+            }
+        })
+    });
 
 
 
